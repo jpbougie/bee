86deaba220711096ed24925be5fe0de0abc942e5
Jean-Philippe Bougie <jp.bougie@gmail.com>
2009-06-20 19:43:51 +0200

    Changed the domain extractor to use tf idf to sort the domains

--- src/main/scala/net/jpbougie/seco/WordnetDomains.scala
+++ src/main/scala/net/jpbougie/seco/WordnetDomains.scala
@@ -1,7 +1,7 @@
 package net.jpbougie.seco
 
 import scala.io.Source
-import scala.collection.immutable.{IntMap, Map}
+import scala.collection.immutable.{IntMap, Map, Set}
 
 import net.jpbougie.bee.{Task, ExecutionProfile, Configurable}
 
@@ -16,6 +16,14 @@ import edu.smu.tspell.wordnet.impl.file.Morphology
 import edu.smu.tspell.wordnet.impl.file.synset._
 
 /* Uses the WordNet database to extract a synset and then WordNet Domains to fetch it most probably domain */
+
+/*
+ * The hierarchy is as follows :
+    In input, this task takes a list of parts of the question, extracted from the previous task
+    Each part is composed of one or more objects, noun which appear directly in the text
+    From each object, we extract its senses from WordNet
+    From each sense that we obtained, we extract one or more domain given by WordNet-Domains
+ */
 class WordnetDomains extends Task with Configurable {
   
   var applyTo = ""
@@ -67,29 +75,61 @@ class WordnetDomains extends Task with Configurable {
     
     var db = WordNetDatabase.getFileInstance
     
-    val objects = (list ! (list ! str))(params(this.applyTo).result.get)
+    val parts = (list ! (list ! str))(params(this.applyTo).result.get)
     
-    def getDomains(word: String): List[String] = {
+    // Returns the list of domains for each sense of a word
+    def getDomains(word: String): List[List[String]] = {
       
+      // get possible variations on a word, as a plural or a singular, and then sanitize them
       val words = ((word :: Nil) ++ Morphology.getInstance.getBaseFormCandidates(word, SynsetType.NOUN)
                                                          .filter(x => !(x equals word)))
                                     .map { _.toLowerCase.replaceAll(" ", "_") }
       
-      for(w <- words if this.synsetsMap contains w;
-          offset <- this.synsetsMap(w);
-          domain <- this.domainMap(offset))
-        yield domain
+      for(w <- words if this.synsetsMap contains w; // for each variation that actually exists in the dictionary
+          offset <- this.synsetsMap(w)) // get the senses of the word
+          yield (for(domain <- this.domainMap(offset))
+            yield domain).toList
     }
     
-    var domains = for(domain <- objects) yield domain.flatMap(getDomains(_))
-                      
-    // extract the frequency of each domain, sort them by frequency
+    // calculates the inverse document frequency for each domain included in a part
+    // for reference (part -> object -> sense -> domain)
+    def idf(part: List[List[List[String]]]): Map[String, Double] = {
+      /*
+       * Formula for idf
+       * idf(i) = log(D/a(i))
+       * where i is a domain, D is the total number of senses for that part
+       * a(i) is the number of senses in which the domain appears
+       */
+       
+      val D = part.length.asInstanceOf[Double]
+      
+      def extractDomainsFromObject(o: List[List[String]]): Set[String] = { 
+        o.foldLeft(Set.empty[String]) { (set:Set[String], sense: List[String]) => (set ++ sense) }
+      }
+      
+      val map = part.foldLeft(Map[String, Int]().withDefaultValue(0)) { (m:Map[String, Int], obj:List[List[String]]) => m ++ extractDomainsFromObject(obj).toList.map { d => (d -> (m(d) + 1))} }
+      
+      map transform {(domain, appearances) => Math.log(D / appearances.asInstanceOf[Double]) }
+    }
     
-    var sortedDomains = for(domain <- domains)
-                          yield domain.foldLeft(Map[String, Int]().withDefaultValue(0))
-                                                { (map: Map[String, Int], obj: String) => map + (obj -> (map(obj) + 1))}.toList.sort(_._2 > _._2).map(_._1)
+    def tf(part: List[List[List[String]]]): Map[String, Double] = {
+      val numberOfDomains = part map { obj => obj.foldLeft(0)(_ + _.length) } // returns the total number of domains for each object
+      
+      part.zip(numberOfDomains).foldLeft(Map[String, Double]().withDefaultValue(0.0)) { (m: Map[String, Double], tuple: (List[List[String]], Int)) =>
+          def foldDomains(map: Map[String, Double], dom: String, nbrDomains: Int): Map[String, Double] = 
+                            { map + (dom -> (map(dom) + (1 / nbrDomains.asInstanceOf[Double]): Double)) }
+          tuple._1.foldLeft(m) { (m: Map[String, Double], sense: List[String]) => (sense.foldLeft(m)(foldDomains(_, _, tuple._2))) }
+        }
+    }
     
-    JsArray(sortedDomains.map(d => { JsArray(d.map(JsString(_))) }))
+    var expandedParts = parts.map { obj => obj.map(getDomains(_)) }
+    
+    val partsWithMaps = expandedParts.map { part => (part, tf(part), idf(part)) }
+    
+    val result = for((p, tf, idf) <- partsWithMaps)
+                  yield tf.toList.map { case (dom, tfScore) => (dom, tfScore * idf(dom)) }.sort(_._2 > _._2).map(_._1)
+    
+    JsArray(result.map(d => { JsArray(d.map(JsString(_))) }))
   }
   
 }
